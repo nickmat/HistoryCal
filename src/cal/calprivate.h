ebd7744aa2da605622cb1f180fa109ac11439fbc
Nick Matthews <nick@kizzyco.com>
2014-11-14 18:32:20 +0000

    Add cal/calprivate.h header file and update build system.
    We need somewhere to hold misc. types and constants that are used generally  but we don't want to expose outside of the library (which caldefs.h is).

---
+++ src/cal/calprivate.h
@@ -0,0 +1,38 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Name:        include/cal/calprivate.h
+ * Project:     Cal: Programmable Historical Calendar library.
+ * Purpose:     Miscellaneous data types and constants not externally visable.
+ * Author:      Nick Matthews
+ * Website:     http://historycal.org
+ * Created:     14th November 2014
+ * Copyright:   Copyright (c) 2014, Nick Matthews.
+ * Licence:     GNU GPLv3
+ *
+ *  The Cal library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The Cal library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Cal library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+*/
+
+#ifndef CAL_CALPRIVATE_H_GUARD
+#define CAL_CALPRIVATE_H_GUARD
+
+#include "cal/caldefs.h"
+
+namespace Cal {
+
+
+}
+
+#endif // CAL_CALPRIVATE_H_GUARD
