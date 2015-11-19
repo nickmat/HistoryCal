ed85189c3ed3fb41f4ac7a4c8d64b854c18e39c0
Nick Matthews <nick@kizzyco.com>
2015-11-19 17:09:34 +0000

    Add new makefile build/gnu_eu/GNUmakefile to build project without utilities and cppunit test programs.

--- build/bake.bat
+++ build/bake.bat
@@ -1,4 +1,6 @@
-bakefile -f gnu -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=gnu -o gnu/GNUmakefile hcal.bkl
+bakefile -f gnu -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=gnu_eu -D EXCLUDE_UTIL=1 -o gnu_eu/GNUmakefile hcal.bkl
+
+bakefile -f gnu -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=gnu -D EXCLUDE_UTIL=0 -o gnu/GNUmakefile hcal.bkl
 
 bakefile -f msvs2008prj -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=vc9 -o vc9/hcal hcal.bkl
 
