de31b10383cfd72a1642958d055b9935c004e721
Nick Matthews <nick@kizzyco.com>
2015-11-21 16:13:25 +0000

    Update build/bake.bat file to correctly make vc9 build files.
    Bakefile requires the EXCLUDE_UTIL variable to be explicitly defined.

--- build/bake.bat
+++ build/bake.bat
@@ -2,6 +2,6 @@ bakefile -f gnu -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=gnu_eu -D EXCL
 
 bakefile -f gnu -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=gnu -D EXCLUDE_UTIL=0 -o gnu/GNUmakefile hcal.bkl
 
-bakefile -f msvs2008prj -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=vc9 -o vc9/hcal hcal.bkl
+bakefile -f msvs2008prj -I %WXWIN%\build\bakefiles\wxpresets -D BUILD_DIR=vc9 -D EXCLUDE_UTIL=0 -o vc9/hcal hcal.bkl
 
 pause
