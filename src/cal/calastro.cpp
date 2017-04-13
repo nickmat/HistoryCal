07d06b5685a4e458a9d11e6ba6398746f0187cc5
Nick Matthews <nick@kizzyco.com>
2017-04-13 15:12:07 +0100

    Cal library, no change - add missing headers to cal/calastro.cpp and cal/calastro.h.

--- src/cal/calastro.cpp
+++ src/cal/calastro.cpp
@@ -1,3 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Name:        src/cal/calastro.cpp
+ * Project:     Cal: Programmable Historical Calendar library.
+ * Purpose:     Functions for astrological calculations.
+ * Author:      Nick Matthews
+ * Website:     http://historycal.org
+ * Created:     19th December 2014
+ * Copyright:   Copyright (c) 2014 ~ 2017, Nick Matthews.
+ * Licence:     GNU GPLv3
+ *
+ *  The Cal library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The Cal library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the Cal library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+*/
 
 #include "calastro.h"
 
